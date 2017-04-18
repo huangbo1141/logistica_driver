4d8c8d6df032ea85dd7ffa21e94a9f8c75c67b7e
BoHuang <bohuang29@hotmail.com>
2017-04-18 09:58:00 +0800

    Initial Commit

---
+++ Logistika/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Logistika
+//
+//  Created by BoHuang on 4/18/17.
+//  Copyright © 2017 BoHuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
